fb00bcb0721cf1436eceaaaea85efdad33fca4b0
Patsakorn Wonghansa <patsakornwonghansa@Patsakorns-MacBook-Pro.local>
2018-04-05 01:07:42 -0500

    Created and connected Navbar skeleton component

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className={Navbar}>
+            hello from navbar.
+        </div>
+    )
+}
+
+export default Navbar;
