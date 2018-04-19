e610f2c86e35662451948a60f6a176443231cbc1
BonnW <pwonghansa@gmail.com>
2018-04-19 16:00:06 -0500

    Fixed styling on Splash.js and started adding images to splash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-<<<<<<< HEAD
-import './components/Navbar/Navbar.css';
-=======
 import Splash from './components/Splash/Splash';
->>>>>>> 3d3205b1f94b03fe6518edb2668712c0a8d7cfcc
+
+import './components/Navbar/Navbar.css';
+
+
+
+/******** Testing Some shit ********/
+
+function SayHello(props) {
+  return (
+    <div>
+      Hello {props.firstName} {props.lastName}!
+    </div>
+  )
+}
+
+const PropTypes = {
+  string(props, propName, componentName) {
+    if (typeof props[propName] !== 'string') {
+      return new Error(`You fucked up`) // DON'T KEEP THIS -- REMOVE BEFORE MERGE
+    }
+  }
+}
+
+SayHello.propTypes = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+}
+
+/******* Testing Some shit END ********/
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        {/* <SayHello firstName={true} lastName={false} />, */}
         <Splash />
+        {/* <Navbar /> */}
+
       </div>
     );
   }
--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -8,37 +8,42 @@ import {
 } from 'reactstrap';
 
 import './Splash.css';
+import Logo from '../../srcIMGs/Logo.png';
 
 
 export default class Splash extends React.Component { 
-    constructor(props) {
-        super();
+  constructor(props) {
+    super();
 
-        this.state = {
+      this.state = {
 
-        }
-    }
+      }
+  }
 
-    render() {
-        return (
-            <div className="Root--Component">
-                <div className="Header"> 
-                    <div className="Header--Title">Thipi Thai</div>
-                    <div className="Header--subTitle">Restaurant & Bar</div>
-                    <Form>
-                        <FormGroup>
-                            <Label for="exampleEmail">Email</Label>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="Your Email" />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="examplePass">Password</Label>
-                            <Input type="password" name="password" id="examplePassword" placeholder="Your Password" />
-                        </FormGroup>
-                    </Form>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Root--Component">
+        <div className="Header"> 
+          <div className="ThipiLogo--Container">
+            <img className="ThipiLogo--image" src={Logo} alt="ThipiLogo" />
+          </div>
+          <div className="Header--Title">Thipi Thai</div>
+          <div className="Header--subTitle">Restaurant & Bar</div>
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input type="email" name="email" id="exampleEmail" placeholder="Your Email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePass">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="Your Password" />
+            </FormGroup>
+          </Form>
+          <button className="GuestButton">Continue as Guest</button>
+        </div>
+      </div>
+    )
+  }
 
 
 }
