bc6bd180383e8b7a02c4ce8119bcdb7c35cee4ec
BonnW <pwonghansa@gmail.com>
2018-04-19 21:57:23 -0500

    Created login form component

---
+++ frontend/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+
+} from 'reactstrap';
+
+import './Splash.css';
+import Logo from '../../srcIMGs/Logo.png';
+
+
+export default class Splash extends React.Component { 
+  constructor(props) {
+    super();
+
+      this.state = {
+
+      }
+  }
